651e423943d293ad2097a4c41596c868c528ba24
Chloe Kuo <42684149+ckuo23@users.noreply.github.com>
2018-10-01 15:21:29 -0400

    Create milestone2.ino

---
+++ milestone2/milestone2.ino
@@ -0,0 +1,343 @@
+#include <QTRSensors.h>
+#include <Servo.h>
+#include <FFT.h> // include the library
+
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+
+#define NUM_SENSORS             3  // number of sensors used
+#define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
+#define EMITTER_PIN             2  // emitter is controlled by digital pin 2
+
+#define BACK                    0
+#define RIGHT                   1
+#define LEFT                    2
+
+// sensors 0 through 2 are connected to analog inputs 0 through 2, respectively
+QTRSensorsAnalog qtra((unsigned char[]) {0, 1, 2},
+  NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
+unsigned int sensorValues[NUM_SENSORS];
+
+int threshold = 400;
+
+//controller shit
+int linePos = 0;
+int desiredPos = 2000;
+int e_p = 0;
+int prev_e_p = 0;
+int e_d = 0;
+
+//Correction Function Variables
+volatile int leftspeed = 180;
+volatile int rightspeed = 0;
+int corrector = 30;
+
+//State machine variables
+int state;
+int nextstate = 0;
+int waitingcommand = 0;
+int turncommand = 0;
+
+//Variables for figure 8 sequence
+int turnsequence[] = {-1, -1};
+int i = 0;
+
+/********************
+ * ROBOT1 Variables *
+ ********************/
+Servo wheelRight;
+Servo wheelLeft;
+
+
+void setup()
+{    
+
+  // FFT stuff
+  
+  Serial.begin(115200); // use the serial port
+
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  
+  wheelRight.attach(5); // wheels
+  wheelLeft.attach(6);
+
+  wheelRight.write(90);
+  wheelLeft.write(90);
+  
+  delay(1000);
+}
+
+
+void loop() {
+  while(1) { // reduces jitter
+    cli();  // UDRE interrupt slows this way down on arduino1.0
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    if(fft_log_out[42] >= 45){
+      digitalWrite(LED_BUILTIN, HIGH);
+      if(waitingcommand == 1){
+        turncommand = turnsequence[i];
+        i = (i == 1)? 0 : i + 1;
+        waitingcommand = 0;
+      }      
+    }
+    else{
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    
+    correctionMachine();
+    wheelRight.write(rightspeed);
+    wheelLeft.write(leftspeed);
+    delay(5);
+
+  }
+}
+
+//constrain(analog, ANALOG_MIN, 255);
+
+ /******************
+ * Path Correction *
+ ******************/
+ 
+void correctionMachine(){
+  state = nextstate;
+  linePos = getLinePos();
+  switch (state){
+    case 0:
+      correctStraight();
+      
+      switch(linePos){
+        case 0:
+          nextstate = 9;
+          break;
+        case 1000:
+          nextstate = 1;
+          break;
+        case 2000:
+          nextstate = 0;
+          break;
+        case 3000:
+          nextstate = 2;
+          break;
+        case 4000:
+          nextstate = 3;
+          break;
+        default:
+          nextstate = 0;
+          break;
+      }
+      break;
+      
+    case 1:
+      correctLeft();
+      nextstate = (linePos == 1000)? 1 : ((linePos == 4000) ? 3 : 0);
+      break;
+
+    case 2:
+      correctRight();
+      nextstate = (linePos == 3000)? 2 : ((linePos == 4000) ? 3 : 0);
+      break;
+      
+    case 3:
+      correctStraight();
+      delay(250);
+      nextstate = 4;
+      //nextstate = (linePos == 4000)? 3 : 4;
+      break;
+      
+    case 4:
+      halt();
+      waitingcommand = 1;
+      if(turncommand == 0){
+        nextstate = 4;
+      }
+      else{
+        waitingcommand = 0;
+        nextstate = (turncommand == -1) ? 5 : 7;
+      }
+      break;
+      
+    case 5:
+      turnRight();
+      turncommand = 0;
+      nextstate = (linePos == 3000) ? 6 : 5;
+      break;
+
+    case 6:
+      turnRight();
+      if(linePos == 2000){
+        halt();
+        nextstate = 0;
+      }
+      else{
+        nextstate = 6;
+      }
+      break;
+
+    case 7:
+      turnLeft();
+      turncommand = 0;
+      nextstate = (linePos == 1000) ? 8 : 7;
+      break;
+
+    case 8:
+      turnLeft();
+      if(linePos == 2000){
+        halt();
+        nextstate = 0;
+      }
+      else{
+        nextstate = 8;
+      }
+      break;
+      
+    case 9:
+      correctStraight();
+      switch(linePos){
+        case 0:
+          nextstate = 9;
+          break;
+        case 1000:
+          nextstate = 10;
+          break;
+        case 2000:
+          nextstate = 0;
+          break;
+        case 3000:
+          nextstate = 11;
+          break;
+        default:
+          nextstate = 0;
+          break;
+      }
+      break;
+
+    case 10:
+      correctRight();
+      nextstate = (linePos == 1000)? 10 : 0;
+      break;
+      
+    case 11:
+      correctLeft();
+      nextstate = (linePos == 3000)? 11 : 0;
+      break;
+      
+    case 12:
+      //implement later
+      break;
+  }
+
+  
+}
+void correctStraight(){
+  if(rightspeed > 0){
+    rightspeed -= corrector;
+  }
+  if(leftspeed < 180){
+  leftspeed += corrector;
+  }
+}
+
+void correctLeft(){
+  if(rightspeed > 0){
+    rightspeed -= corrector;
+  }
+  else if(rightspeed == 0 && leftspeed > 90){
+    leftspeed -= corrector;
+  }
+}
+
+void correctRight(){
+  if(leftspeed < 180){
+    leftspeed += corrector;
+  }
+  else if(leftspeed == 180 && rightspeed < 90){
+    rightspeed += corrector;
+  }
+}
+
+/******************
+ * ROBOT1 Methods *
+ ******************/
+
+void turnLeft(){
+  rightspeed = 0;
+  leftspeed = 0;
+}
+
+void turnRight(){
+  rightspeed = 180;;
+  leftspeed = 180;
+}
+
+void halt() {
+  rightspeed = 90;
+  leftspeed = 90;
+}
+
+ /*********************
+ * Position Estimator *
+ *********************/
+int getLinePos(){
+  qtra.read(sensorValues);
+  int LP, L, B, R, sum;
+  L = (sensorValues[LEFT] < threshold)? 100 : 0;
+  B = (sensorValues[BACK] < threshold)? 10 : 0;
+  R = (sensorValues[RIGHT] < threshold)? 1 : 0;
+  sum = L + B + R;
+  switch(sum){
+    case 000:
+      LP = 2000;
+      break;
+    case 001:
+      LP = 3000;
+      break;
+    case 011:
+      LP = 3000;
+      break;
+    case 010:
+      LP = 2000;
+      break;
+    case 100:
+      LP = 1000;
+      break;
+    case 110:
+      LP = 1000;
+      break;
+    case 111:
+      LP = 4000;
+      break;
+    case 101:
+      LP = linePos;
+      break;
+    default:
+      LP = 2000;
+      break; 
+  }
+  return LP;
+}
+
+
+
