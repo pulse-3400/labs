788a965f8777919fdb24aad3da833a2f5ff39e43
Chloe Kuo <42684149+ckuo23@users.noreply.github.com>
2018-12-02 12:22:23 -0500

    Create Stack.h

---
+++ milestone3/Stack.h
@@ -0,0 +1,35 @@
+// December 1, 2018
+
+class Stack {
+  int stack[81]; // stores directions to closest unvisited node
+  
+  public: 
+    // TODO: constructor
+    Stack() {
+      clearStack(0);
+    }
+
+    // clears stack but puts current location as first position
+    void clearStack(int location) {
+      stack[0] = location;
+      for (int k = 1; k < 81; k++) {
+        stack[k] = -1;
+      }
+    }
+
+    void updateLocation(int current) {
+      checkUnvisited();
+      for (int k = 0; k < 81; k++) {
+        if (k == -1) {
+          stack[k] = current;
+        }
+      }
+    }
+
+    // NOTE the direction the robot decides to go should be passed in as false
+    void checkUnvisited(int currentLocation, int n, int e, int s, int w) {
+      if (n+e+s+w >= 2) { //cc if there is more than one unvisited node
+        clearStack(currentLocation);
+      }
+    } 
+};
